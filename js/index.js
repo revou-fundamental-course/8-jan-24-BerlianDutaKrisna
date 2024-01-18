7ab05d6534fec29519cadc2dcf7cb7b814431bbf
BerlianDutaKrisna <berliandutakrisna@gmail.com>
2024-01-18 17:39:55 +0700

    tampilan responsive

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function luasPersegi() { 
+    // Access the button element by its ID 
+    var button = document.getElementById("luasPersegiId"); 
+ 
+    // Retrieve the value of the button 
+    var buttonValue = button.value; 
+ 
+    // Do something with the button value 
+    console.log("Button value: " + buttonValue); 
+    // You can now use the buttonValue variable in your JavaScript code 
+} 
